5ab27191d311a7a5f4df2c9b4f0c7301b2b3fd60
Officialsly <153915032+Officialsly@users.noreply.github.com>
2023-12-15 13:48:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2349046585239', 'sly', true],
+  ['2349046585239', 'sly', true], 
   [''] 
 ] //Number of owners
 
